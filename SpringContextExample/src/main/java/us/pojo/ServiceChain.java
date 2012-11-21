a377b4dfcadf000be7d64b8c2114d335d0bf980e
benlamonica <ben.lamonica@gmail.com>
2012-11-20 21:05:42 -0600

    Add additional commentary in the spring-context.xml, and logging in ServiceChain.

--- SpringContextExample/src/main/java/us/pojo/ServiceChain.java
+++ SpringContextExample/src/main/java/us/pojo/ServiceChain.java
@@ -3,8 +3,13 @@ package us.pojo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ServiceChain implements Service {
 
+	private static final Logger log = LoggerFactory.getLogger(ServiceChain.class);
+	
 	private List<Service> services;
 	
 	public ServiceChain(List<Service> services) {
@@ -12,11 +17,13 @@ public class ServiceChain implements Service {
 	}
 	
 	public String doSomething() {
+		log.info("Service Chain starting.");
 		StringBuilder buf = new StringBuilder();
 		for (Service service : services) {
 			buf.append(service.doSomething());
 		}
 
+		log.info("Service Chain finished.");
 		return buf.toString();
 	}
 
