f4dbd891dfc70faa4e9251d8b63bec726a6ff3d0
ravibabuporuri <ravibabuporuri@gmail.com>
2012-11-20 21:19:42 -0600

    ravi changed "hello service" to "hello services ben"

--- SpringContextExample/src/main/java/us/pojo/Main.java
+++ SpringContextExample/src/main/java/us/pojo/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
 		
-		Service helloService = context.getBean("HelloService", Service.class);
+		Service helloService = context.getBean("HelloService Ben", Service.class);
 		Service goodbyeService = context.getBean("GoodbyeService", Service.class);
 		Service conversationService = context.getBean("ConversationService", Service.class);
 		helloService.doSomething();
